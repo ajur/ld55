f1964a4c57df15a26d92bc360c9845b4891a2500
Adam Jurczyk <ajur.pl@gmail.com>
2024-04-16 14:17:28 +0200

    fixed bug with double music sometimes

--- src/main.js
+++ src/main.js
@@ -33,7 +33,7 @@ if (!isDebugOn) {
   onResize(app.screen.width, app.screen.height);
 
   popup({text: "Summoning the Genie\n\n1. find oil lamp or glass bottle\n2. rub it!\n3. summon the Genie!!!", modal: true, onClick: () => {
-    sound.play("audio/market_music.mp3", {volume: 0.5, loop: true});
+    sound.play("audio/market_music.mp3", {volume: 0.5, loop: true, singleInstance: true});
     game.newGame();
   }});
 
